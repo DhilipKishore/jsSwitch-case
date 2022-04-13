f9794cd0f6f2437cb6320439f09f17f28ea436ff
DhilipKishore <99136490+DhilipKishore@users.noreply.github.com>
2022-04-13 18:06:06 +0530

    Add files via upload

---
+++ jsSwitch.js
@@ -0,0 +1,16 @@
+
+let mark = 60;
+switch(true){
+    case mark > 90 :
+        console.log("Super grade");
+        break;
+    case mark > 50 :
+        console.log("Pass");
+        break;
+    case mark < 50 :
+        console.log("Fail");
+        break;
+    default :
+    console.log("Unknown mark");
+
+}
